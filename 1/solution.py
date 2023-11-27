f01c2d9f572ad991cfc39b1249f4b60bc3183f8a
ivan maksimov <i.maksimov@npo-echelon.ru>
2023-11-27 18:51:58 +0300

    solution 1

--- 1/solution.py
+++ 1/solution.py
@@ -3,18 +3,46 @@ from types import TracebackType
 
 class Solution:
     def __init__(self) -> None:
-        self.__dirty_water_volume: int = 0
+        self.__dirty_water_count: int = 0
         self.__cleen_water_count: int = 0
         self.__total_water_count: int = 0
+        self.current_water_in_cleen_volume: int = 0
+        self.overflow: bool = False
 
     def _read_user_input(self) -> str:
         return input()
 
     def _parse_user_input(self, user_input: str) -> None:
-        user_input = user_input.strip().split()
+        dirty_water, cleen_water, total_water = user_input.strip().split()
+        self.dirty_water_count = dirty_water
+        self.cleen_water_count = cleen_water
+        self.total_water_count = total_water
+
+    def _check_for_zero_result(self) -> bool:
+        return True if self.__cleen_water_count >= self.__dirty_water_count else False
+
+    def _check_for_instantaneous_overflow(self) -> bool:
+        return True if self.__total_water_count < self.__dirty_water_count else False
+
+    def _calculate_overflow_result(self) -> int:
+        days = 0
+        while not self.overflow:
+            days += 10
+            self.current_water_in_cleen_volume += (
+                2 * self.dirty_water_count
+            ) - self.cleen_water_count
+            if self.current_water_in_cleen_volume > self.__total_water_count:
+                self.overflow = True
+        return days
 
     def __call__(self) -> int:
         user_input = self._read_user_input()
+        self._parse_user_input(user_input)
+        if self._check_for_instantaneous_overflow():
+            return 3
+        if self._check_for_zero_result():
+            return 0
+        return self._calculate_overflow_result()
 
     def __enter__(self):
         return self
@@ -28,12 +56,12 @@ class Solution:
         ...
 
     @property
-    def dirty_water_volume(self) -> int:
-        return self.__dirty_water_volume
+    def dirty_water_count(self) -> int:
+        return self.__dirty_water_count
 
-    @dirty_water_volume.setter
-    def dirty_water_volume(self, value) -> None:
-        self.__dirty_water_volume = value
+    @dirty_water_count.setter
+    def dirty_water_count(self, value) -> None:
+        self.__dirty_water_count = int(value)
 
     @property
     def cleen_water_count(self) -> int:
@@ -41,7 +69,7 @@ class Solution:
 
     @cleen_water_count.setter
     def cleen_water_count(self, value) -> None:
-        self.__cleen_water_count = value
+        self.__cleen_water_count = int(value)
 
     @property
     def total_water_count(self) -> int:
@@ -49,9 +77,9 @@ class Solution:
 
     @total_water_count.setter
     def total_water_count(self, value) -> None:
-        self.__total_water_count = value
+        self.__total_water_count = int(value)
 
 
 if __name__ == "__main__":
     with Solution() as solution:
-        solution()
+        print(solution())
